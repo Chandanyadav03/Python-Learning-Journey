be5b609870eed12d78b31219e7ae1d7f856b92c2
Chandanyadav03 <cy2706837@gmail.com>
2026-06-06 12:15:19 +0530

    rules

---
+++ Chapter-2/03_rules_variables.py
@@ -0,0 +1,14 @@
+## Rules for Defining a Variable Name
+
+# Valid Variable Names
+name = "Vipul"
+age = 20
+student_1 = "Rahul"
+_marks = 85
+one8 = 18
+seven = 7
+
+# Invalid Variable Names
+# 1name = "Vipul"      # Starts with a digit
+# my name = "Vipul"    # Contains space
+# student@1 = "Rahul"  # Contains special character
