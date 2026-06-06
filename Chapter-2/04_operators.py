10cdebc194b4c2a48dd15be4978d028920c8c262
Chandanyadav03 <cy2706837@gmail.com>
2026-06-06 14:44:35 +0530

    operators file

---
+++ Chapter-2/04_operators.py
@@ -0,0 +1,27 @@
+#  Arithmetic Operators +, -, *, /, %, **, //
+
+a = 10
+b= 10
+c = a+b
+print(c)
+
+# Assignment Operators =, +=, -=, *=, /=, %=, **=, //=
+a = 6
+a +=3
+print(a)
+
+# Comparison Operators ==, !=, >, <, >=, <=
+
+d = 10<5
+print(d)
+
+e = 10<=10
+print(e)
+
+
+# Logical Operators and, or, not
+f = 1 or 5 
+print(f)
+
+g = 1 and 0
+print(g)
