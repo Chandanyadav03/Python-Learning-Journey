4a5f6ad591f4ea69ac0bd893f16dc9bb62e0991d
Chandanyadav03 <cy2706837@gmail.com>
2026-06-06 15:36:16 +0530

    PS file

---
+++ Chapter-2/PS/06_problem6.py
@@ -0,0 +1,6 @@
+# 6. Write a Python program to calculate the square of a number entered by the user.
+
+a = int(input("Enter a number:"))
+
+print("Square of a number is:", a*a)
+print("Square of a number is:", a**3)
