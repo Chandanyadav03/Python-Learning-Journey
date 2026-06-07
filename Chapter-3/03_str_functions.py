c7e997b0e38093e33840227b6c5d678f657e56fa
Chandanyadav03 <cy2706837@gmail.com>
2026-06-07 15:59:48 +0530

    Update

---
+++ Chapter-3/03_str_functions.py
@@ -0,0 +1,9 @@
+name = "chandan yadav"
+print(len(name)) 
+print(name.endswith("yadav"))
+print(name.startswith("Cha")) # This function is case sensitive
+print(name.capitalize())  #starting letter will be capital and rest will be small
+
+
+print(name.split())
+print("-".join(name))
