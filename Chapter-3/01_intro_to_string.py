e8d3d6ee12646fc040952476a852722b235bf8a1
Chandanyadav03 <cy2706837@gmail.com>
2026-06-07 15:36:38 +0530

    Update

--- Chapter-3/01_intro_to_string.py
+++ Chapter-3/01_intro_to_string.py
@@ -1,3 +1,8 @@
 a = 'chandan'
 b = "chandan"
 c = '''chandan'''
+
+d = a[0:5] # start form index 0 to index 4
+print(d)
+e = a[1:3]
+print(e)
---
+++ Chapter-3/02_negative_slicing.py
@@ -0,0 +1,7 @@
+name = "chandan"
+
+print(name[-1])  # Output: n
+print(name[-3:-1])  # Output: na
+print(name[4:6])  # Output: an
+print(name[:5])
+print(name[1:])
