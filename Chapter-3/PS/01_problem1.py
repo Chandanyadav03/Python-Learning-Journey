c5148e8df722771fa9d17f1f5a5ef473cd651919
Chandanyadav03 <cy2706837@gmail.com>
2026-06-08 12:33:06 +0530

    Update PS

---
+++ Chapter-3/PS/01_problem1.py
@@ -0,0 +1,4 @@
+# 1. Write a Python program to display a user-entered name followed by **"Good Afternoon"** using the `input()` function.
+
+name = input("Enter Your Name: ")
+print(f"Good Afternoon, {name}")
