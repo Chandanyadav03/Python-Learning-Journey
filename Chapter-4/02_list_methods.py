0f612385e4cc73c7a51891decf06f2a619eb54b0
Chandanyadav03 <cy2706837@gmail.com>
2026-06-08 15:56:57 +0530

    Update chapter 2

---
+++ Chapter-4/02_list_methods.py
@@ -0,0 +1,15 @@
+hello = ["Apple", 0.25,10, "Orange","Chandan"]
+print(hello)
+
+hello.append("yadav")
+print(hello)
+
+l1 = [1,50,32,60,500,10]
+# l1.sort()
+#l1.reverse()
+# l1.insert(4, 100)
+# print(l1.pop(2))
+# print(l1)
+
+l1.remove(1)
+print(l1)
