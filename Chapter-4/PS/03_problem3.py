35de9cf8de0e8d7894761af90b4ae1e6059536b2
Chandanyadav03 <cy2706837@gmail.com>
2026-06-20 14:41:52 +0530

    chap 4

---
+++ Chapter-4/PS/03_problem3.py
@@ -0,0 +1,6 @@
+### Question 3
+# Check that a tuple type cannot be changed in Python.
+
+
+a = (50,800,"chandan")
+a[2] = "yadav"
---
+++ Chapter-4/PS/04_problem4.py
@@ -0,0 +1,2 @@
+### Question 4
+# Write a program to sum a list with 4 numbers.
