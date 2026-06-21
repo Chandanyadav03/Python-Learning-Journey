2605d9355c9a139e09f422238de76167cf44ce94
Chandanyadav03 <cy2706837@gmail.com>
2026-06-21 14:50:02 +0530

    chapter 5

---
+++ Chapter-5/02_dict_methods.py
@@ -0,0 +1,13 @@
+marks = {
+    "chandan": 98,
+    "yadav": 93,
+    "alice": 80,
+     0 : "bob"
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+
+marks.update({"chandan" : 99, "vipul" : 85})
+print(marks)
